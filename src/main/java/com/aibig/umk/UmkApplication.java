4273d0ad262775eb084d935a0767aa1901ebf71c
Ieskandar Zulqarnain <ieskandarzulqarnain@graduate.utm.my>
2023-09-27 16:28:37 +0800

    Starter

---
+++ src/main/java/com/aibig/umk/UmkApplication.java
@@ -0,0 +1,13 @@
+package com.aibig.umk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aibig/umk/UmkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibig.umk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
